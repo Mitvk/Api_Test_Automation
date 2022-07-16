ac8b2187dd77be6d8f4a5764d42ec4c8a2ab5fe5
Mitvk <mvinodkumar.inbox@gmail.com>
2022-07-16 12:24:05 +0530

    Added user level login test

--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -7,3 +7,9 @@ def test_login():
     response = Auth().login(APP_URL, "admin", "admin")
     LOG.info(response.json())
     assert response.ok
+
+def test_user_login():
+    LOG.info('test_user_login')
+    response = Auth().login(APP_URL,"user","user")
+    LOG.info(response.json())
+    assert response.ok
